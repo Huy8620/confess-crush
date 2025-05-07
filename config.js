afddc2d7d94af04830fdbc2d6a4e3acaeb73ae9f
Huy8620 <kzlhuylzk@gmail.com>
2025-05-08 00:27:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Anh Là HuyBui đẹp zai đâyyyy",
+    introTitle: 'Anh yêu em Nguyễn Thị Mai Hạnh ',
+    introDesc: `Anh yêu em Nguyễn Thị Mai Hạnh`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    title: 'Em có yêu anh Không  🥰',
+    desc: 'Em có yêu anh Không ',
+    btnYes: 'EMMM CÓ <33',
+    btnNo: 'Không  :3',
+    question: 'Tại sao lại yêu anh  <3',
+    btnReply: 'Gửi cho anh Huy đẹp zai  <3',
+    reply: 'Vì anh đẹp trai',
+    mess: 'Anh biết mà 🥰. Anh yêu em  nhiều nhiều 😘😘',
+    messDesc: 'Tối mai 7h15, anh qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
