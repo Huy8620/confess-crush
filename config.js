c30e78ca5668bb3b1531aef3198fd46f3b31b89a
Huy8620 <kzlhuylzk@gmail.com>
2025-05-08 00:21:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Bùi Quang Huy yêu Mai Hạnh ",
+    introTitle: 'Huy yêu Hạnh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
